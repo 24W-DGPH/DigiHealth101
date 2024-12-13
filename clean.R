ec72d566d04edf831799349337d4d6f27ee25a0f
Rakshak671100 <rakshak.manandhar@stud.th-deg.de>
2024-12-13 09:28:02 +0100

    change

---
+++ clean.R
@@ -0,0 +1,11 @@
+2+3
+sum(-12,99,76,-78)
+(200+550+2*925)*1.1-420
+sum(99,89,-27,-456)
+# Load packages ----------
+pacman::p_load(
+  rio, # importing data
+  here, # relative file pathways
+  styler, # source code formatting
+  lintr, # detects bad code patterns
+)
