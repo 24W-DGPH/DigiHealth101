7ed6d2d6a79d1d61908543076c5b0a3664a77f17
Rakshak671100 <rakshak.manandhar@stud.th-deg.de>
2025-01-24 10:21:34 +0100

    change

--- mental_health.R
+++ mental_health.R
@@ -11,15 +11,12 @@ pacman::p_load(
   lubridate, # working with dates
   epikit, # age_categories() function
   matchmaker, # dictionary based cleaning
-  group_by, # groups one or more variables
   tidyverse, # data management and visualizations
   styler, # source code formatting
   lintr, # detects bad code pattern
   skimr, # preview tibbles
   ggplot2, # data visualization
   zoo, # extra date functions
-  as.Date, #date manipulation
-  as.POSIXct, #date manipulation
   plotly,   #interactive plots
   shiny   # shiny app
 )
